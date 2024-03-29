cf12d51503bc23ce92fa5088191a49694ac84e71
alya <162738913+bendelibiralya@users.noreply.github.com>
2024-03-29 19:23:05 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+from botlogic import gen_pass
+from botlogic import yazi_tura
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$parola'):
+        parola = gen_pass(10)
+        await message.channel.send(parola)
+    elif message.content.startswith('$yazitura'):
+        oyun = yazi_tura()
+        await message.channel.send(oyun)
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run(your discord code)
