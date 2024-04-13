6058d3a4d86be9ec3611234845eb294da0380b9f
alya <162738913+bendelibiralya@users.noreply.github.com>
2024-04-13 18:27:02 +0300

    Update kittypic.py

--- kittypic.py
+++ kittypic.py
@@ -17,4 +17,15 @@ async def kedy(ctx):
     await ctx.send("KEDY!")
     await ctx.send(file=pickitty)
 #you can find the cat images i used in '!(kittyimages_link)' in my profile
+
+@bot.command()
+async def uzgunkedy(ctx):
+    uzgunler_list = os.listdir("uzgunkedyler")
+    uzgunler_name = random.choice(uzgunler_list)
+    with open(f'uzgunkedyler/{uzgunler_name}','rb') as f:
+        uzgunfoti = discord.File(f)
+    await ctx.send("SAD!")
+    await ctx.send(file=uzgunfoti)
+#sad kitty images r on my account under the name "!(sadcatimages_link)"
+
 bot.run("your token here")
