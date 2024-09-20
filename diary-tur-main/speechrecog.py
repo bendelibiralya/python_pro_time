57f7e166c434dc007219cada94a1f57783c07093
alya <162738913+bendelibiralya@users.noreply.github.com>
2024-09-20 19:04:02 +0300

    Speech_recognition game

---
+++ diary-tur-main/speechrecog.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+
+def speech_eng():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say the word!")
+        audio = r.listen(source, timeout=20)
+    text = r.recognize_google(audio)
+    return text
+
+if __name__ == "__main__":
+    # Burada fonksiyonu test edebilirsin
+    print(speech_eng())
+
+def speech_tr():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source, timeout=20)
+    text = r.recognize_google(audio, language="tr-TR")
+    return text
+
